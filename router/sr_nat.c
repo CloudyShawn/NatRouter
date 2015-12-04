1f8c247add0e7ae76e111909c63a55de7572fdf5
Shawn Krivorot <shawn.krivorot@hotmail.com>
2015-12-04 12:31:40 -0500

    shortened function

--- router/sr_nat.c
+++ router/sr_nat.c
@@ -596,35 +596,11 @@ struct sr_nat_connection *nat_connection_lookup(struct sr_nat *nat, struct sr_na
 
 
 int isFlag(sr_tcp_hdr_t *tcp_hdr) {
-  if (tcp_hdr->tcp_flags & TCP_FIN) {
-    return 1;
-  }
-
-  if (tcp_hdr->tcp_flags & TCP_SYN) {
-    return 1;
-  }
-
-  if (tcp_hdr->tcp_flags & TCP_RST) {
-    return 1;
-  }
-
-  if (tcp_hdr->tcp_flags & TCP_PSH) {
-    return 1;
-  }
-
-  if (tcp_hdr->tcp_flags & TCP_ACK) {
-    return 1;
-  }
-
-  if (tcp_hdr->tcp_flags & TCP_URG) {
-    return 1;
-  }
-
-  if (tcp_hdr->tcp_flags & TCP_ECE) {
-    return 1;
-  }
-
-  if (tcp_hdr->tcp_flags & TCP_CWR) {
+  if (tcp_hdr->tcp_flags & TCP_FIN || tcp_hdr->tcp_flags & TCP_SYN ||
+      tcp_hdr->tcp_flags & TCP_RST || tcp_hdr->tcp_flags & TCP_PSH ||
+      tcp_hdr->tcp_flags & TCP_ACK || tcp_hdr->tcp_flags & TCP_URG ||
+      tcp_hdr->tcp_flags & TCP_ECE || tcp_hdr->tcp_flags & TCP_CWR)
+  {
     return 1;
   }
 
